39242ec57aba7150520fe2c7edfd7891ddc8e07f
eapo <e@eaposztrof.com>
2017-07-16 12:41:28 +0200

    Version 1.0 release
    - updated content
    - redesign and
    - [Steemit.com CustomSubmit +Steem it! BookMarklet](https://github.com/eapo/steemit-custom-scripts/blob/master/Steem-it-BookMarklet.user.js) compatibility

--- CustomSubmit-SteemRadio.user.js
+++ CustomSubmit-SteemRadio.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Steemit.com CustomSubmit +SteemRadio
-// @description   Easier to post to @SteemRadio's #musictrends01 contest
+// @description   Easier to post to @SteemRadio's #musictrends contest
 // @author        eaposztrof
 // @updateURL     https://raw.githubusercontent.com/eapo/steemit-custom-scripts/master/CustomSubmit-SteemRadio.user.js
 // @downloadURL   https://raw.githubusercontent.com/eapo/steemit-custom-scripts/master/CustomSubmit-SteemRadio.user.js
@@ -13,17 +13,46 @@
 // @include       https://*.steemit.com/*
 // @require       https://code.jquery.com/jquery-1.8.3.js
 // @run-at        document-start
-// @version       0.2
+// @version       1.0
 // ==/UserScript==
 
 (function() {
+    var musictrends="musictrends02"; // edit the number here when it change
+    var content = [
+        "- - -",
+        "<center><sup>This is my entry for @steemradio's #"+musictrends+" - [\"Win STEEM and SBD just for posting your favorite music!\"](https://steemit.com/"+musictrends+"/@steemradio/music-trends-on-steem-radio-01-winners-announced-the-start-of-the-music-trends-on-steemradio-02)</sup>",
+        "Browse some of the @steemradio tags: <br>[dance](https://steemit.com/created/steemradio-dance) | [pop](https://steemit.com/created/steemradio-pop) | [rock](https://steemit.com/created/steemradio-rock) | [latino](https://steemit.com/created/steemradio-latin) | [funk](https://steemit.com/created/steemradio-funk) | [alternative](https://steemit.com/created/steemradio-alternative) | [hip hop](https://steemit.com/created/steemradio-hiphop) | [electronic](https://steemit.com/created/steemradio-electronic) | [chill](https://steemit.com/created/steemradio-chill) | [reggae](https://steemit.com/created/steemradio-reggae) | [indie](https://steemit.com/created/steemradio-indie) | [country](https://steemit.com/created/steemradio-country) | [r&b/soul](https://steemit.com/created/steemradio-rnbsoul) | [jazz](https://steemit.com/created/steemradio-jazz) | [world](https://steemit.com/created/steemradio-world) | [dubstep](https://steemit.com/created/steemradio-dubstep) | [metal](https://steemit.com/created/steemradio-metal) | [powermetal](https://steemit.com/created/powermetal) | [edm](https://steemit.com/created/steemradio-edm) | [classical](https://steemit.com/created/steemradio-classical)",
+        "<br>[![steemradio-logo-eapo-128px.png](https://steemitimages.com/DQmZw62hL2jcvyaXCD6EugiU4xMWqhVZi1QHLUyzKKY8xLB/steemradio-logo-eapo-128px.png)](https://steemit.com/steemradio/@eaposztrof/customsubmit-steemradio-custom-script-for-steemit-com)<br><sub>created quickly with the new<sup>v1</sup> [CustomSubmit +SteemRadio script](https://steemit.com/steemradio/@eaposztrof/customsubmit-steemradio-custom-script-for-steemit-com) by @eaposztrof</sub></center>",
+        "",
+    ].join("\n");
     var css = [
         "/* @steemradio */",
         ".App__content {",
         " margin-top: 1em;",
         "}",
         "/* /steemradio */",
+        ".SidePanel>div ul:nth-of-type(n+3) {",
+        " margin-top: 1px;",
+        "}",
+        ".SidePanel>div .menu>li>a {",
+        " padding: 0.3em 0.8em;",
+        "}",
+        ".SidePanel>div .menu>li>a {",
+        " padding: 0.3em 0.8em;",
+        "}",
+        ".SidePanel .menu {",
+        " -ms-flex-wrap: wrap !important;",
+        " flex-wrap: wrap !important;",
+        "border-bottom: #fff 1px solid;",
+        "}",
+        ".SidePanel .menu>li {",
+        " -webkit-box-flex: 0;",
+        " -ms-flex: 0 0 100% !important;",
+        " flex: 0 0 100% !important;",
+        "}",
     ].join("\n");
+
+    console.log("%cSteemit.com %cCustomSubmit %c+SteemRadio! CustomScript %cby @eaposztrof", "color:#1a5099; font-family:Source Sans Pro,Helvetica Neue,Helvetica,Arial,sans-serif; font-size:large", "font-style: italic;font-size:large","color:#1a5099; font-family:Source Sans Pro,Helvetica Neue,Helvetica,Arial,sans-serif; font-size:large","font-family:consolas,courier");
 
     if (typeof GM_addStyle != "undefined") {
         GM_addStyle(css);
@@ -44,25 +73,31 @@
         }
     }
 
+    $.fn.getCursorPosition = function () {
+        var el = $(this).get(0);
+        var pos = 0;
+        if ('selectionStart' in el) {
+            pos = el.selectionStart;
+        } else if ('selection' in document) {
+            el.focus();
+            var Sel = document.selection.createRange();
+            var SelLength = document.selection.createRange().text.length;
+            Sel.moveStart('character', -el.value.length);
+            pos = Sel.text.length - SelLength;
+        }
+        return pos;
+    };
+
     jQuery(document).ready(function($) {
 
-        var mainMenu = $('ul.HorizontalMenu.menu>li').detach();
-        mainMenu.appendTo('.Header__top.header > .row > .columns:first-child > ul.menu');
+        var mainMenu = $('.Header ul.HorizontalMenu.menu > *').detach();
+        mainMenu.appendTo('.Header__top .columns:first-child ul.menu');
 
         // @steemradio
         if (window.location.href.indexOf("submit.html") > -1) {
-            $('.Header__top.header .columns:first-child ul.menu > li:first-child').fadeIn(200).attr('id','steemradio').html('<a href="#steemradio"><img src="https://steemitimages.com/132x48/https://steemitimages.com/DQmNvNEqwbFjYAL5FbJTVPRj3uFeqDxQ8Ap6q7ZEH7uQcKt/steemradio-logo-eapo.gif" width="132" height="48"></a>');
+            $('li.Header__top-logo').fadeIn(200).attr('id','steemradio').html('<a href="#steemradio"><img src="https://steemitimages.com/132x48/https://steemitimages.com/DQmNvNEqwbFjYAL5FbJTVPRj3uFeqDxQ8Ap6q7ZEH7uQcKt/steemradio-logo-eapo.gif" width="132" height="48"></a>');
         } else {
         }
-        var content = [
-            "<center>[![steemradio-logo-eapo.gif](https://steemitimages.com/DQmNvNEqwbFjYAL5FbJTVPRj3uFeqDxQ8Ap6q7ZEH7uQcKt/steemradio-logo-eapo.gif)](https://steemit.com/steemradio/@eaposztrof/customsubmit-steemradio-custom-script-for-steemit-com)<br><sub>generated with the new [CustomSubmit +SteemRadio scrip](https://steemit.com/steemradio/@eaposztrof/customsubmit-steemradio-custom-script-for-steemit-com) by @eaposztrof</sub></center>",
-            "paste URL here",
-            "- - -",
-            "This is my entry for @steemradio's #musictrends01 - [\"Win STEEM and SBD just for posting your favorite music!\"](https://steemit.com/musictrends01/@steemradio/music-trends-on-steem-radio-01-post-your-favorite-music-and-win-steem-sbd)",
-            "- - -",
-            "Browse some of the @steemradio tags: [dance](https://steemit.com/created/steemradio-dance) | [pop](https://steemit.com/created/steemradio-pop) | [rock](https://steemit.com/created/steemradio-rock) | [latino](https://steemit.com/created/steemradio-latin) | [funk](https://steemit.com/created/steemradio-funk) | [alternative](https://steemit.com/created/steemradio-alternative) | [hip hop](https://steemit.com/created/steemradio-hiphop) | [electronic](https://steemit.com/created/steemradio-electronic) | [chill](https://steemit.com/created/steemradio-chill) | [reggae](https://steemit.com/created/steemradio-reggae) | [indie](https://steemit.com/created/steemradio-indie) | [country](https://steemit.com/created/steemradio-country) | [r&b/soul](https://steemit.com/created/steemradio-rnbsoul) | [jazz](https://steemit.com/created/steemradio-jazz) | [world](https://steemit.com/created/steemradio-world) | [dubstep](https://steemit.com/created/steemradio-dubstep) | [metal](https://steemit.com/created/steemradio-metal) | [powermetal](https://steemit.com/created/powermetal) | [edm](https://steemit.com/created/steemradio-edm) | [classical](https://steemit.com/created/steemradio-classical)",
-            "",
-        ].join("\n");
 
         $('.MarkdownViewer.Markdown > div a[href^="https://steemit.com/created/"]').live('click', function(e) {
             e.preventDefault();
@@ -70,13 +105,18 @@
             if(cat.val().indexOf('steemradio') > -1){
                 cat.val(cat.val()+" "+this.href.replace('https://steemit.com/created/','')+'').focus();
             } else {
-                cat.val("steemradio-music music musictrends01 "+cat.val()+this.href.replace('https://steemit.com/created/','')+'').focus();
+                cat.val("steemradio-music music "+musictrends+" "+cat.val()+this.href.replace('https://steemit.com/created/','')+'').focus();
             }
         });
 
+        var textarea=$('.SubmitPost textarea');
+
         $('#steemradio > *').live('click', function(e) {
             e.preventDefault();
-            $('.SubmitPost textarea').val(content).focus().get(0).setSelectionRange(428,442);
+            var position = textarea.getCursorPosition();
+            var oriContent = textarea.val();
+            var newContent = oriContent.substr(0, position) + "\n"+ content +"\n" + oriContent.substr(position);
+            textarea.val(newContent).focus().get(0).setSelectionRange(0,0);
         });
 
         // /steemradio
